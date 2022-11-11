6c6c067ca225e2667cff0b01d66eed99fae34df3
truong-ngo <truong.ngo1993@gmail.com>
2022-11-11 14:22:34 +0700

    add

--- Module-2/src/map/binary_tree/AbstractTree.java
+++ Module-2/src/map/binary_tree/AbstractTree.java
@@ -3,7 +3,6 @@ package map.binary_tree;
 public abstract class AbstractTree<E> implements Tree<E> {
     public void inorder() {
     }
-
     public void postorder() {
     }
     public void preorder() {
--- Module-2/src/map/binary_tree/Tree.java
+++ Module-2/src/map/binary_tree/Tree.java
@@ -3,6 +3,7 @@ package map.binary_tree;
 public interface Tree<E> {
     boolean insert(E e);
     boolean delete(E e);
+    boolean search(E e);
     void inorder();
     void postorder();
     void preorder();
